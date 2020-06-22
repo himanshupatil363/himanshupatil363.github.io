cd78ea9b544180c110e60bcd253769292316d4a6
himanshupatil363 <60534916+himanshupatil363@users.noreply.github.com>
2020-06-22 15:09:38 +0530

    nav

---
+++ main.js
@@ -0,0 +1,23 @@
+function slide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".na");
+    const navlinks = document.querySelectorAll(".na li");
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("na-active");
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.8s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        burger.classList.toggle("toggle");
+    });
+}
+slide();
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()> 600);
+});
+
+
+  
